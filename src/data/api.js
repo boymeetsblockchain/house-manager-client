a97ecde6922e2eff95ed69d58f709b88f4c9793a
Aje Oluwasegun Victor <105326765+boymeetsblockchain@users.noreply.github.com>
2025-07-13 17:22:48 +0100

    Update api.js

--- src/data/api.js
+++ src/data/api.js
@@ -1,2 +1,2 @@
-// export const API = "https://house-manager-api.onrender.com/api/";
-export const API = "http://localhost:5000/api/";
+export const API = "https://house-manager-api.onrender.com/api/";
+// export const API = "http://localhost:5000/api/";
